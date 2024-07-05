bd2840ccb7b63b458eb99008b37345976eb265c6
Ivan Kabanov <i.borkoff@yandex.ru>
2024-07-05 12:58:23 +0300

    add icons for error and add templates for WeatherAppTest

---
+++ app/src/androidTest/java/com/example/weatherapp/WeatherAppTest.kt
@@ -0,0 +1,33 @@
+package com.example.weatherapp
+
+import androidx.test.espresso.Espresso.pressBack
+import androidx.test.ext.junit.rules.ActivityScenarioRule
+import androidx.test.ext.junit.runners.AndroidJUnit4
+import org.junit.Assert.*
+import org.junit.Rule
+import org.junit.Test
+import org.junit.runner.RunWith
+
+@RunWith(AndroidJUnit4::class)
+class WeatherAppTest {
+    @get:Rule
+    var activityScenarioRule = ActivityScenarioRule(MainActivity::class.java)
+
+    @Test
+    fun test_launch_app_without_internet_connection(){
+        // check main error
+        // check empty recyclerViews
+        // check error find city
+    }
+    @Test
+    fun test_launch_app_without_geo() {
+        // check main error
+        // check empty recyclerViews
+        // check find city, choose city and comeback
+    }
+    @Test
+    fun test_launch_app_without_problems() {
+        // check not empty recyclerViews
+        // change city and comeback
+    }
+}
