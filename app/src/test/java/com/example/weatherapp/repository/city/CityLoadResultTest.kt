9635c4eb23dd994c7fec8f3e941af5170650adaf
Ivan Kabanov <i.borkoff@yandex.ru>
2024-07-11 21:52:19 +0300

    complete LoadResultTests

--- app/src/test/java/com/example/weatherapp/repository/city/CityLoadResultTest.kt
+++ app/src/test/java/com/example/weatherapp/repository/city/CityLoadResultTest.kt
@@ -1,69 +1,157 @@
 package com.example.weatherapp.repository.city
 
-//import com.example.weatherapp.city.FakeCityGeoLiveDataWrapper
-//import com.example.weatherapp.city.FakeCityListLiveDataWrapper
-//import com.example.weatherapp.core.FakeLiveDataWrapper
-//import com.example.weatherapp.core.LiveDataWrapper
-//import com.example.weatherapp.core.LoadResult
-//import com.example.weatherapp.core.UiState
-//import org.junit.Test
-//
-//class CityLoadResultTest {
-//    @Test
-//    fun test_success() {
-//        val cityNameCityLoadResult = LoadResult.CityNameSuccess(
-//            data = listOf(
-//                CityResponse(
-//                    name = "a",
-//                    lat = 1.1,
-//                    lon = 1.1,
-//                    country = "aa",
-//                    state = "aaa"
-//                )
-//            )
-//        )
-//        val citiesListLiveDataWrapper = FakeLiveDataWrapper.Base()
-//        val citiesListLiveDataWrapperUpdate : LiveDataWrapper.Update = citiesListLiveDataWrapper
-//        cityNameCityLoadResult.show(
-//            updateLiveData = citiesListLiveDataWrapperUpdate
-//        )
-//        citiesListLiveDataWrapper.checkUpdateCalls(
-//            listOf(
-//                UiState.CitiesListDataShow(
-//                    listOf(
-//                        CityResponse(
-//                            name = "a",
-//                            lat = 1.1,
-//                            lon = 1.1,
-//                            country = "aa",
-//                            state = "aaa"
-//                        )
-//                    ),
-//                    noConnection = null
-//                )
-//            )
-//        )
-//        val cityGeoCityLoadResult = LoadResult.CityGeoSuccess(
-//            CityResponse(
-//                name = "b",
-//                lat = 1.2,
-//                lon = 1.2,
-//                country = "bb",
-//                state = "bbb"
-//            )
-//        )
-//        val cityGeoLiveDataWrapperUpdate : LiveDataWrapper.Update = cityGeoLiveDataWrapper
-//        cityGeoCityLoadResult.show(
-//            updateCityGeoLiveData = cityGeoLiveDataWrapperUpdate
-//        )
-//        cityGeoLiveDataWrapper.checkUpdateCalls(
-//            CityResponse(
-//                name = "b",
-//                lat = 1.2,
-//                lon = 1.2,
-//                country = "bb",
-//                state = "bbb"
-//            )
-//        )
-//    }
-//}
+import com.example.weatherapp.core.FakeLiveDataWrapper
+import com.example.weatherapp.core.LiveDataWrapper
+import com.example.weatherapp.core.LoadResult
+import com.example.weatherapp.core.UiState
+import org.junit.Test
+
+class CityLoadResultTest {
+    @Test
+    fun test_success() {
+        val cityNameCityLoadResult = LoadResult.CityNameSuccess(
+            data = listOf(
+                CityResponse(
+                    name = "a",
+                    lat = 1.1,
+                    lon = 1.1,
+                    country = "aa",
+                    state = "aaa"
+                )
+            )
+        )
+        val cityLiveDataWrapper = FakeLiveDataWrapper.Base()
+        val liveDataWrapperUpdate : LiveDataWrapper.Update = cityLiveDataWrapper
+        cityNameCityLoadResult.show(
+            updateLiveData = liveDataWrapperUpdate
+        )
+        cityLiveDataWrapper.checkUpdateCalls(
+            listOf(
+                UiState.CitiesListDataShow(
+                    data = listOf(
+                        CityResponse(
+                            name = "a",
+                            lat = 1.1,
+                            lon = 1.1,
+                            country = "aa",
+                            state = "aaa"
+                        )
+                    ),
+                    noConnection = null
+                )
+            )
+        )
+        val cityGeoCityLoadResult = LoadResult.CityGeoSuccess(
+            data = CityResponse(
+                name = "b",
+                lat = 1.2,
+                lon = 1.2,
+                country = "bb",
+                state = "bbb"
+            )
+        )
+        cityGeoCityLoadResult.show(
+            updateLiveData = liveDataWrapperUpdate
+        )
+        cityLiveDataWrapper.checkUpdateCalls(
+            listOf(
+                UiState.CitiesListDataShow(
+                    data = listOf(
+                        CityResponse(
+                            name = "a",
+                            lat = 1.1,
+                            lon = 1.1,
+                            country = "aa",
+                            state = "aaa"
+                        )
+                    ),
+                    noConnection = null
+                ),
+                UiState.CityGeoDataShow(
+                    data = CityResponse(
+                        name = "b",
+                        lat = 1.2,
+                        lon = 1.2,
+                        country = "bb",
+                        state = "bbb"
+                    ),
+                    noConnection =  null
+                )
+            )
+        )
+    }
+    @Test
+    fun test_no_connection(){
+        val cityNameCityLoadResult = LoadResult.CityNameError(
+            noConnection = true
+        )
+        val cityLiveDataWrapper = FakeLiveDataWrapper.Base()
+        val liveDataWrapperUpdate : LiveDataWrapper.Update = cityLiveDataWrapper
+        cityNameCityLoadResult.show(
+            updateLiveData = liveDataWrapperUpdate
+        )
+        cityLiveDataWrapper.checkUpdateCalls(
+            listOf(
+                UiState.CitiesListDataShow(
+                    data = null,
+                    noConnection = true
+                )
+            )
+        )
+        val cityGeoCityLoadResult = LoadResult.CityGeoError(
+            noConnection = true
+        )
+        cityGeoCityLoadResult.show(
+            updateLiveData = liveDataWrapperUpdate
+        )
+        cityLiveDataWrapper.checkUpdateCalls(
+            listOf(
+                UiState.CitiesListDataShow(
+                    data = null,
+                    noConnection = true
+                ),
+                UiState.CityGeoDataShow(
+                    data = null,
+                    noConnection =  true
+                )
+            )
+        )
+    }
+    @Test
+    fun test_other(){
+        val cityNameCityLoadResult = LoadResult.CityNameError(
+            noConnection = false
+        )
+        val cityLiveDataWrapper = FakeLiveDataWrapper.Base()
+        val liveDataWrapperUpdate : LiveDataWrapper.Update = cityLiveDataWrapper
+        cityNameCityLoadResult.show(
+            updateLiveData = liveDataWrapperUpdate
+        )
+        cityLiveDataWrapper.checkUpdateCalls(
+            listOf(
+                UiState.CitiesListDataShow(
+                    data = null,
+                    noConnection = false
+                )
+            )
+        )
+        val cityGeoCityLoadResult = LoadResult.CityGeoError(
+            noConnection = false
+        )
+        cityGeoCityLoadResult.show(
+            updateLiveData = liveDataWrapperUpdate
+        )
+        cityLiveDataWrapper.checkUpdateCalls(
+            listOf(
+                UiState.CitiesListDataShow(
+                    data = null,
+                    noConnection = false
+                ),
+                UiState.CityGeoDataShow(
+                    data = null,
+                    noConnection =  false
+                )
+            )
+        )
+    }
+}
---
+++ app/src/test/java/com/example/weatherapp/repository/weather/WeatherLoadResultTest.kt
@@ -0,0 +1,244 @@
+package com.example.weatherapp.repository.weather
+
+import com.example.weatherapp.core.FakeLiveDataWrapper
+import com.example.weatherapp.weatherScreen.CurrentWeatherData
+import com.example.weatherapp.weatherScreen.FutureWeatherData
+import com.example.weatherapp.core.UiState
+import com.example.weatherapp.weatherScreen.TodayWeatherData
+import com.example.weatherapp.core.LiveDataWrapper
+import com.example.weatherapp.core.LoadResult
+import org.junit.Test
+
+class WeatherLoadResultTest {
+    @Test
+    fun test_success() {
+        val currentLoadResult = LoadResult.CurrentWeatherSuccess(
+            data = CurrentWeatherData(
+                icon = "icon",
+                degrees = 0.0,
+                windSpeed = 0.0,
+                windDeg = 0,
+                precip = Pair(0.0, ""),
+                date = 0L
+            )
+        )
+        val weatherLiveDataWrapper = FakeLiveDataWrapper.Base()
+        val liveDataWrapperUpdate : LiveDataWrapper.Update = weatherLiveDataWrapper
+        currentLoadResult.show(
+            updateLiveData = liveDataWrapperUpdate
+        )
+        weatherLiveDataWrapper.checkUpdateCalls(listOf(
+            UiState.CurrentWeatherDataShow(
+                data = CurrentWeatherData(
+                    icon = "icon",
+                    degrees = 0.0,
+                    windSpeed = 0.0,
+                    windDeg = 0,
+                    precip = Pair(0.0, ""),
+                    date = 0L
+                ),
+                noConnection = null
+            )
+        ))
+
+        val todayLoadResult = LoadResult.TodayWeatherSuccess(
+            data = listOf(
+                TodayWeatherData(
+                    icon = "icon",
+                    date = 0L,
+                    degrees = 0.0,
+                )
+            )
+        )
+        todayLoadResult.show(
+            updateLiveData = liveDataWrapperUpdate
+        )
+        weatherLiveDataWrapper.checkUpdateCalls(
+            listOf(
+                UiState.CurrentWeatherDataShow(
+                    data = CurrentWeatherData(
+                        icon = "icon",
+                        degrees = 0.0,
+                        windSpeed = 0.0,
+                        windDeg = 0,
+                        precip = Pair(0.0, ""),
+                        date = 0L
+                    ),
+                    noConnection = null
+                ),
+                UiState.TodayWeatherDataShow(
+                    listOf(
+                        TodayWeatherData(
+                            icon = "icon",
+                            date = 0L,
+                            degrees = 0.0,
+                        )
+                    ), null
+                )
+            )
+        )
+        val futureWeatherResult = LoadResult.FutureWeatherSuccess(
+            data = listOf(
+                FutureWeatherData(
+                    icon = "icon",
+                    date = 0L,
+                    minDegrees = 0.0,
+                    maxDegrees = 0.0
+                )
+            )
+        )
+        futureWeatherResult.show(
+            updateLiveData = liveDataWrapperUpdate
+        )
+        weatherLiveDataWrapper.checkUpdateCalls(
+            listOf(
+                UiState.CurrentWeatherDataShow(
+                    data = CurrentWeatherData(
+                        icon = "icon",
+                        degrees = 0.0,
+                        windSpeed = 0.0,
+                        windDeg = 0,
+                        precip = Pair(0.0, ""),
+                        date = 0L
+                    ),
+                    noConnection = null
+                ),
+                UiState.TodayWeatherDataShow(
+                    listOf(
+                        TodayWeatherData(
+                            icon = "icon",
+                            date = 0L,
+                            degrees = 0.0,
+                        )
+                    ),
+                    noConnection = null
+                ),
+                UiState.FutureWeatherDataShow(
+                    data = listOf(
+                        FutureWeatherData(
+                            icon = "icon",
+                            date = 0L,
+                            minDegrees = 0.0,
+                            maxDegrees = 0.0
+                        )
+                    ),
+                    noConnection = null
+                )
+            )
+        )
+    }
+    @Test
+    fun test_no_connection() {
+        val currentLoadResult = LoadResult.CurrentWeatherError(
+            noConnection = true
+        )
+        val weatherLiveDataWrapper = FakeLiveDataWrapper.Base()
+        val liveDataWrapperUpdate : LiveDataWrapper.Update = weatherLiveDataWrapper
+        currentLoadResult.show(
+            updateLiveData = liveDataWrapperUpdate
+        )
+        weatherLiveDataWrapper.checkUpdateCalls(listOf(
+            UiState.CurrentWeatherDataShow(
+                data = null,
+                noConnection = true
+            )
+        ))
+        val todayLoadResult = LoadResult.TodayWeatherError(
+            noConnection = true
+        )
+        todayLoadResult.show(
+            updateLiveData = liveDataWrapperUpdate
+        )
+        weatherLiveDataWrapper.checkUpdateCalls(
+            listOf(
+                UiState.CurrentWeatherDataShow(
+                    data = null,
+                    noConnection = true
+                ),
+                UiState.TodayWeatherDataShow(
+                    data = null,
+                    noConnection = true
+                )
+            )
+        )
+        val futureLoadResult = LoadResult.FutureWeatherError(
+            noConnection = true
+        )
+        futureLoadResult.show(
+            updateLiveData = liveDataWrapperUpdate
+        )
+        weatherLiveDataWrapper.checkUpdateCalls(
+            listOf(
+                UiState.CurrentWeatherDataShow(
+                    data = null,
+                    noConnection = true
+                ),
+                UiState.TodayWeatherDataShow(
+                    data = null,
+                    noConnection = true
+                ),
+                UiState.FutureWeatherDataShow(
+                    data = null,
+                    noConnection = true
+                )
+            )
+        )
+    }
+    @Test
+    fun test_other() {
+        val currentLoadResult = LoadResult.CurrentWeatherError(
+            noConnection = false
+        )
+        val weatherLiveDataWrapper = FakeLiveDataWrapper.Base()
+        val liveDataWrapperUpdate : LiveDataWrapper.Update = weatherLiveDataWrapper
+        currentLoadResult.show(
+            updateLiveData = liveDataWrapperUpdate
+        )
+        weatherLiveDataWrapper.checkUpdateCalls(listOf(
+            UiState.CurrentWeatherDataShow(
+                data = null,
+                noConnection = false
+            )
+        ))
+        val todayLoadResult = LoadResult.TodayWeatherError(
+            noConnection = false
+        )
+        todayLoadResult.show(
+            updateLiveData = liveDataWrapperUpdate
+        )
+        weatherLiveDataWrapper.checkUpdateCalls(
+            listOf(
+                UiState.CurrentWeatherDataShow(
+                    data = null,
+                    noConnection = false
+                ),
+                UiState.TodayWeatherDataShow(
+                    data = null,
+                    noConnection = false
+                )
+            )
+        )
+        val futureLoadResult = LoadResult.FutureWeatherError(
+            noConnection = false
+        )
+        futureLoadResult.show(
+            updateLiveData = liveDataWrapperUpdate
+        )
+        weatherLiveDataWrapper.checkUpdateCalls(
+            listOf(
+                UiState.CurrentWeatherDataShow(
+                    data = null,
+                    noConnection = false
+                ),
+                UiState.TodayWeatherDataShow(
+                    data = null,
+                    noConnection = false
+                ),
+                UiState.FutureWeatherDataShow(
+                    data = null,
+                    noConnection = false
+                )
+            )
+        )
+    }
+}
